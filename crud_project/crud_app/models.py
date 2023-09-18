8014211e9bec7dbcab412d2a300d96c919520632
Jerilyn Yare <102701655+Jy-re@users.noreply.github.com>
2023-09-18 10:18:22 +0800

    initial commit and added initial model

---
+++ crud_project/crud_app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Models
+
+class Flowers(models.Model):
+    name = models.CharField(max_length=200)
+    scientificname = models.CharField(max_length=200)
+    season = models.CharField(max_length=200)
+    habitat = models.CharField(max_length=200)
