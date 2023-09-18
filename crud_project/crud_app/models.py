c15ec518640daa427fbc8c9d8beda8e5a3ae6490
Jerilyn Yare <102701655+Jy-re@users.noreply.github.com>
2023-09-18 10:19:42 +0800

    Added description to model

--- crud_project/crud_app/models.py
+++ crud_project/crud_app/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-# Models
+# Models:
 
 class Flowers(models.Model):
     name = models.CharField(max_length=200)
     scientificname = models.CharField(max_length=200)
     season = models.CharField(max_length=200)
     habitat = models.CharField(max_length=200)
+    description = models.TextField()
