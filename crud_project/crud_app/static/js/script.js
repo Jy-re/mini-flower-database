fe9f6f4780682faecc0dd00ffe7e1eb776f14855
Jerilyn Yare <102701655+Jy-re@users.noreply.github.com>
2023-09-20 18:47:05 +0800

    we'll see if this is fine

--- crud_project/crud_app/static/js/script.js
+++ crud_project/crud_app/static/js/script.js
@@ -1,12 +1,15 @@
-// Wrap all your JavaScript code in a DOMContentLoaded event listener
 document.addEventListener("DOMContentLoaded", function () {
-    // Move the code inside this event listener
     var modal = document.getElementById("myModal");
     var addButton = document.getElementById("btnAdd");
+    var cancelButton = document.getElementById("cancelButton");
     var flowerForm = document.getElementById("flowerForm");
   
     addButton.addEventListener("click", function () {
       modal.style.display = "block";
+    });
+  
+    cancelButton.addEventListener("click", function () {
+      modal.style.display = "none";
     });
   
     window.addEventListener("click", function (event) {
@@ -40,30 +43,5 @@ document.addEventListener("DOMContentLoaded", function () {
           console.error("Error:", error);
         });
     });
-
-    // Add an event listener for each "Edit" button
-    document.querySelectorAll('.edit-button').forEach(function(editButton) {
-        editButton.addEventListener('click', function(e) {
-            e.preventDefault();
-            const targetModalId = this.getAttribute('data-target');
-            const editModal = document.querySelector(targetModalId);
-            editModal.style.display = 'block';
-        });
-    });
-
-    // Add an event listener for the edit modal's cancel button (similar to add modal)
-    const editCancelButton = document.getElementById("editCancelButton");
-    editCancelButton.addEventListener("click", function () {
-        const editModals = document.querySelectorAll('.modal');
-        editModals.forEach(function(editModal) {
-            editModal.style.display = "none";
-        });
-    });
-
-    // Add an event listener for clicking outside the edit modal (similar to add modal)
-    window.addEventListener("click", function (event) {
-        if (event.target.classList.contains('modal')) {
-            event.target.style.display = 'none';
-        }
-    });
-});
+  });
+  
--- crud_project/crud_app/urls.py
+++ crud_project/crud_app/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.flowers, name='flowers'),
     path('addmodal/', views.add_flower, name='add_flower'),
-    path('editflower/<int:flower_id>/', views.edit_flower, name='edit_flower'),
 
 ]
